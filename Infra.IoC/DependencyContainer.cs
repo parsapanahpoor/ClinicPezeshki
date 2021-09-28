a80ffcf2dba28c6ed510726901df9c2551812a6b
parsapanahpoor <80142438+parsapanahpoor@users.noreply.github.com>
2021-09-28 14:02:36 +0330

    Start Of Project

---
+++ Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            #region Application Layer
+
+            //service.AddScoped<IUserService, UserService>();
+ 
+            #endregion
+
+
+            #region Data Layer
+
+            //service.AddScoped<IUserRepository, UserRepository>();
+
+            #endregion
+
+        }
+    }
+
+}
