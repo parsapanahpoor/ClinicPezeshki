f7d85b8d54441731adfac1726f27db52766f7186
parsapanahpoor <80142438+parsapanahpoor@users.noreply.github.com>
2021-09-29 21:41:47 +0330

    Inital Users Table

---
+++ Infra.Data/Context/ClinicContext.cs
@@ -0,0 +1,60 @@
+﻿using Domain.Models.Permissions;
+using Domain.Models.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class ClinicContext : DbContext
+    {
+        public ClinicContext(DbContextOptions<ClinicContext> options) : base(options)
+        {
+
+        }
+
+
+        #region User
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UsersRoles { get; set; }
+
+        #endregion
+
+        #region Permission
+
+        public DbSet<Permission> Permission { get; set; }
+        public DbSet<RolePermission> RolePermission { get; set; }
+
+        #endregion
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            var cascadeFKs = modelBuilder.Model.GetEntityTypes()
+                .SelectMany(t => t.GetForeignKeys())
+                .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade);
+
+            foreach (var fk in cascadeFKs)
+                fk.DeleteBehavior = DeleteBehavior.Restrict;
+
+
+            #region QueryFilter
+
+            modelBuilder.Entity<User>()
+                .HasQueryFilter(u => !u.IsDelete);
+
+            modelBuilder.Entity<Role>()
+               .HasQueryFilter(u => !u.IsDelete);
+
+            #endregion
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
